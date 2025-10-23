6bf6cc25f19f42d76d8e024c79877e6a04c246f0
DemonQilin <velezloaizaesteban9012015@gmail.com>
2025-10-22 21:16:46 -0500

    Initial commit

---
+++ API.Movies/DAL/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using API.Movies.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Movies.DAL
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        // Section to create the DbSet of entities/models
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ API.Movies/DAL/Models/AuditBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Movies.DAL.Models
+{
+    public class AuditBase
+    {
+        [Key]
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedAt { get; set; }
+        public virtual DateTime UpdatedAt { get; set; }
+    }
+}
